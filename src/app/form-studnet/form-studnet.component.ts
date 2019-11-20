7515c8488697a3d984c64ba8153cf9e70b177b71
ui-ramesh <42331666+ui-ramesh@users.noreply.github.com>
2019-11-20 06:25:09 -0600

    creating student
    Signed-off-by: ui-ramesh <42331666+ui-ramesh@users.noreply.github.com>

--- src/app/form-studnet/form-studnet.component.ts
+++ src/app/form-studnet/form-studnet.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+// import { User } from '../model/studentForm';
 
 @Component({
   selector: 'app-form-studnet',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-studnet.component.scss']
 })
 export class FormStudnetComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  // studentRegisterForm: FormGroup;
+  // user: User = {
+  //   firstName: '',
+  //   lastName: '',
+  //   StudentID : ''
+  // };
+ 
+  constructor(
+    //private formBuilder: FormBuilder
+    ) { 
   }
+ ngOnInit() {
+   // this.formDetail();
+  }
+
+//   formDetail(){
+//   this.studentRegisterForm = this.formBuilder.group({
+//     'firstName': [this.user.firstName, [Validators.required]],
+//     'lastName': [this.user.lastName, [Validators.required]],
+//     'StudentID': [this.user.StudentID, 
+//       [Validators.required,  Validators.minLength(6)]]
+//   });
+// }
 
 }
